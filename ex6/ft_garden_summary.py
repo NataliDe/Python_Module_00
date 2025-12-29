e841a62e893570c14ea966b6e659014541085d68
Natalia Demkiv <mnatao1@gmail.com>
2025-12-29 17:26:06 +0100

    last

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,6 @@
+def ft_garden_summary():
+    a = input("Enter garden name: ")
+    b = int(input("Enter number of plants: "))
+    print("Garden:", a)
+    print("Plants:", b)
+    print("Status: Growing well!")
